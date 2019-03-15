2e581eab1d916fa689b14ded09cbbf7a0d541fa1
ksteimel <ksteimel@iu.edu>
2019-03-15 00:43:55 -0400

    Added sparse array example

--- src/LogisticRegression.jl
+++ src/LogisticRegression.jl
@@ -3,6 +3,8 @@ using LinearAlgebra
 using Distributions
 using Random
 using UnicodePlots
+using SparseArrays
+
 function sigmoid(score::T) where T <: Number
     return 1/(1 + exp(-score))
 end
@@ -25,7 +27,8 @@ function logistic_regression(feats, labels; num_steps=20000, learning_rate=1e-2,
     end
     # should have a weight for each column
     weights = zeros(size(feats)[2]) 
-    
+    scores = zeros(size(feats)[2])
+    error_signal = zeros(size(feats)[2])
     for step=1:num_steps
         scores = feats * weights
         predictions = par_sigmoid(scores)
@@ -42,16 +45,34 @@ function logistic_regression(feats, labels; num_steps=20000, learning_rate=1e-2,
 end
 function test_log_reg()
     num_instances = 5000
-
-    x1_v1 = rand(Normal(0, 2), num_instances)
-    x1_v2 = rand(Normal(0, .3), num_instances)
-    x2_v1 = rand(Normal(4, 1), num_instances)
-    x2_v2 = rand(Normal(2, .3), num_instances)
+    # generate dense training data
+    #x1_v1 = rand(Normal(0, 2), num_instances)
+    #x1_v2 = rand(Normal(0, .3), num_instances)
+    #x2_v1 = rand(Normal(4, 1), num_instances)
+    #x2_v2 = rand(Normal(2, .3), num_instances)
+    
+    # generate sparse training data
+    x1_v1 = sparse(rand(Binomial(4, .2), num_instances))
+    x1_v2 = sparse(rand(Binomial(8, .1),
+    num_instances))
+    x2_v1 = sparse(rand(Binomial(4, .8), num_instances))
+    x2_v2 = sparse(rand(Binomial(8, .7), num_instances))
+    
     x1 = [x1_v1 x1_v2]
     x2 = [x2_v1 x2_v2]
+    # generate sparse training data
+    x3_v1 = sparse(rand(Binomial(4, .2), num_instances))
+    x3_v2 = sparse(rand(Binomial(8, .1),
+    num_instances))
+    x4_v1 = sparse(rand(Binomial(4, .8), num_instances))
+    x4_v2 = sparse(rand(Binomial(8, .7), num_instances))
+    x3 = [x3_v1 x3_v2]
+    x4 = [x4_v1 x4_v2]
+    test_data = [x3 ; x4]
+    test_labels = [zeros(num_instances) ; ones(num_instances)]
     # display the dataset we just made
-    plt = scatterplot(x1[:, 1], x2[:, 2], ylim=[-2,3])
-    scatterplot!(plt, x2[:, 1], x2[:, 2], color=:blue)
+    # plt = scatterplot(x1[:, 1], x2[:, 2], ylim=[-2,3])
+    # scatterplot!(plt, x2[:, 1], x2[:, 2], color=:blue)
     # combine these two separate
     # normal distributions
     data = [x1 ; x2]
@@ -60,8 +81,14 @@ function test_log_reg()
     weights = logistic_regression(data, labels, num_steps=300000, learning_rate=5e-5, add_intercept=true)
     final_scores = data_with_intercept * weights
     preds = round.(par_sigmoid(final_scores))
-    println("Accuracy: ")
+    println("Accuracy on training data: ")
     acc = sum(preds .== labels)/length(labels)
+    println(acc)
+    test_data_with_intercept = [ones(size(test_data)[1]) test_data]
+    test_scores = test_data_with_intercept * weights
+    test_preds = round.(par_sigmoid(test_scores))
+    println("Accuracy on test data: ")
+    acc = sum(test_preds .== test_labels)/length(test_labels)
     println(acc)
     return acc
 end
